69a3c964a7ac7a44b830bd8dbab38427c14f0860
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-06-19 20:56:49 -0700

    onepluscamera: Update to OOS 10.3.11 and update permissions

--- config.mk
+++ config.mk
@@ -5,6 +5,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.xml \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.gallery.xml
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/product/etc/permissions/privapp-permissions-com.oneplus.camera.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/product/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM)/product/etc/permissions/privapp-permissions-com.oneplus.gallery.xml
