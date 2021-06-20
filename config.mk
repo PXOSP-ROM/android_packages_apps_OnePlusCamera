32034daeec71e7f90a6d17553e0258aa6de75ed2
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-06-19 20:56:49 -0700

    onepluscamera: Update to OOS 10.3.11 and update permissions

--- config.mk
+++ config.mk
@@ -4,4 +4,7 @@ PRODUCT_PACKAGES += \
     OnePlusGallery
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
+    $(LOCAL_PATH)/system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.gallery.xml
