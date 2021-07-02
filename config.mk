70af42877680be16243dfac16e9a73a1e34ee64c
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-07-01 22:19:07 -0700

    OOSCam: Move configs to device tree and flatten dir structure

--- config.mk
+++ config.mk
@@ -3,8 +3,3 @@ PRODUCT_PACKAGES += \
     OnePlusCameraService \
     OnePlusGallery
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/product/etc/permissions/privapp-permissions-com.oneplus.camera.xml \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/product/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM)/product/etc/permissions/privapp-permissions-com.oneplus.gallery.xml
