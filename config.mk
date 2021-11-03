20d6dd1a1115c7e19d2b2603dcc3c96e0ab4ddab
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-11-03 08:45:19 -0700

    OnePlusCamera: Move permissions into optional app package

--- config.mk
+++ config.mk
@@ -3,3 +3,8 @@ PRODUCT_PACKAGES += \
     OnePlusCameraService \
     OnePlusGallery
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/OnePlusCamera/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml \
+    $(LOCAL_PATH)/configs/OnePlusCamera/privapp-permissions-com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.xml \
+    $(LOCAL_PATH)/configs/OnePlusCamera/privapp-permissions-com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml \
+    $(LOCAL_PATH)/configs/OnePlusCamera/privapp-permissions-com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.gallery.xml
